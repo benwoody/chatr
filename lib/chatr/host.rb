dd6eca29bf49df649ed1fb230c9c26e6526bc2d3
Ben Woodall <mail@benwoodall.com>
2012-12-08 16:48:11 -0800

    updated how local ip is found

--- lib/chatr/host.rb
+++ lib/chatr/host.rb
@@ -1,8 +1,8 @@
 module Chatr
   class Host
 
-    def initialize(port)
-      local_ip = UDPSocket.open {|s| s.connect("64.233.187.99", 1); s.addr.last}
+    def initialize(port=4242)
+      local_ip = Socket.ip_address_list.last.ip_address
       @serverSocket = TCPServer.new(local_ip,port)
       @serverSocket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, 1)
       puts "Chat server initiated at #{local_ip} on port #{port}"
@@ -38,7 +38,6 @@ module Chatr
       newsock = @serverSocket.accept
       @connections.push(newsock)
       newsock.write("Write EOF to disconnect\n")
-      puts newsock.peeraddr
       str = sprintf "Client #{newsock.peeraddr[2]}:#{newsock.peeraddr[1]} joined.\n"
       broadcast_string(str,newsock)
     end
